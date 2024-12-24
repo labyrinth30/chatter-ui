c113f88424d91d5f4fb0c0edc3712f1fc79aae56
younha00 <ayeon0@office.skhu.ac.kr>
2024-12-24 17:11:22 +0900

    ✨ [#1]Feat: CreateUser mutation 구현 및 proxy로 cors 해결
    회원가입 정상 진행 확인 완료

--- src/components/auth/Signup.tsx
+++ src/components/auth/Signup.tsx
@@ -1,10 +1,23 @@
 import { Link } from "react-router-dom";
 import { Link as MUILink } from "@mui/material";
 import Auth from "./Auth";
+import {useCreateUser} from "../../hooks/useCreateUser";
 
 const Signup = () => {
+    // useCreateUser를 호출하여 반환된 값을 createUser에 할당한다.
+    const [createUser] = useCreateUser();
+
     return (
-        <Auth submitLabel="회원가입하기" onSubmit={async () => { }} >
+        <Auth submitLabel="회원가입하기" onSubmit={async ({ email, password }) => {
+            await createUser({
+                variables: {
+                    createUserInput: {
+                        email,
+                        password
+                    }
+                }
+            })
+        }} >
             <Link to="/login" style={{ alignSelf: "center" }}>
                 <MUILink>로그인 하러가기</MUILink>
             </Link>
