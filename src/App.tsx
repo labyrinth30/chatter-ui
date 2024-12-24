a6ebfe9fdeadd77e0e5798a1ec453e7564c79483
younha00 <ayeon0@office.skhu.ac.kr>
2024-12-24 14:57:17 +0900

    ✨ [#1]Feat: Apollo Client 연동
    환경변수 등록 및 클라이언트 연결

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,27 @@
-import { Container, createTheme, CssBaseline, ThemeProvider } from '@mui/material';
+import {Container, createTheme, CssBaseline, ThemeProvider} from '@mui/material';
 import React from 'react';
-import { RouterProvider } from 'react-router';
+import {RouterProvider} from 'react-router';
 import router from './components/Routes';
+import {ApolloProvider} from "@apollo/client";
+import client from "./constants/apollo-client";
 
 const dartTheme = createTheme({
-  palette: {
-    mode: 'dark',
-  }
+    palette: {
+        mode: 'dark',
+    }
 })
 
 const App = () => {
-  return (
-    <ThemeProvider theme={dartTheme}>
-      <CssBaseline />
-      <Container>
-        <RouterProvider router={router} />
-      </Container>  
-    </ThemeProvider>
-  );
+    return (
+        <ApolloProvider client={client}>
+            <ThemeProvider theme={dartTheme}>
+                <CssBaseline/>
+                <Container>
+                    <RouterProvider router={router}/>
+                </Container>
+            </ThemeProvider>
+        </ApolloProvider>
+    );
 };
 
 export default App;
